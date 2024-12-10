c8c375f5a6d7d5c666282bb1947a21949be2daae
Abibola <73500258+abbie-bola@users.noreply.github.com>
2024-12-10 18:49:37 +0000

    Create motion_sensor_pseudocode.tf

---
+++ motion_sensor_pseudocode.tf
@@ -0,0 +1,5 @@
+train model to identify a human hand in different positions and angles
+  - obtain a large dataset of images (remember to set global seed, for reproducibility)
+test it out
+
+might require action recognition training
