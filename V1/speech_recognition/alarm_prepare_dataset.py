2fe379601391057460718e0a4bb6f23c885dd3ef
Abibola <73500258+abbie-bola@users.noreply.github.com>
2025-01-25 01:27:58 +0000

    Update alarm_prepare_dataset.py
    deleted a block of commented-out code

--- V1/speech_recognition/alarm_prepare_dataset.py
+++ V1/speech_recognition/alarm_prepare_dataset.py
@@ -1,21 +1,3 @@
-# from audiomentations import Compose, AddGaussianNoise, PitchShift, HighPassFilter
-# # HighPassFilter requires pydub 
-# import librosa
-# import soundfile as sf
-
-# augment = Compose([
-#     AddGaussianNoise(min_amplitude=0.1, max_amplitude=0.2, p=1),
-#     PitchShift(min_semitones=-8, max_semitones=8, p=1),
-#     HighPassFilter(min_cutoff_freq=2000, max_cutoff_freq=4000, p=1)
-# ])
-
-# if __name__ == '__main__':
-#     signal, sr = librosa.load("blues.00000.wav")
-#     augmented_signal = augment(signal, sr)
-#     sf.write("augmented.wav", augmented_signal, sr) # save augmented_signal
-
-
-
 import librosa 
 import os
 import json 
